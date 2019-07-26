0de3a7ad84444ae5d139a3e7bac3b75ef1d65c11
NSchoenle <schoenle@grinnell.edu>
2019-07-26 15:25:48 -0500

    Add files via upload

---
+++ src/com/yash/ppmtoolapp/domain/Admin.java
@@ -0,0 +1,22 @@
+package com.yash.ppmtoolapp.domain;
+
+public class Admin {
+	
+	private int user_id;
+	
+	
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	
+	
+	
+	
+	
+
+}
