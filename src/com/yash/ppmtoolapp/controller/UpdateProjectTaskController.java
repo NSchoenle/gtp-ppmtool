514bcdc31dfb94590b314af8962b7eb9c9342abe
NSchoenle <nmihicschoenle@gmail.com>
2019-07-29 08:48:30 -0500

    Fix url-pattern for UpdateProjectTaskController which was causing server error.

--- src/com/yash/ppmtoolapp/controller/UpdateProjectTaskController.java
+++ src/com/yash/ppmtoolapp/controller/UpdateProjectTaskController.java
@@ -17,7 +17,7 @@ import com.yash.ppmtoolapp.serviceimpl.ProjectTaskServiceImpl;
 /**
  * Servlet implementation class UpdateProjectController
  */
-@WebServlet("/UpdateProjectController")
+@WebServlet("/UpdateProjectTaskController")
 public class UpdateProjectTaskController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     private ProjectTaskService projectTaskService = null;
